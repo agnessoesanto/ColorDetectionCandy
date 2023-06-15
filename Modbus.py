556534666b1c9583cb1b7885559ce17f9693fa0e
Asus <soesantoagnes@gmai.com>
2023-06-15 13:54:02 +0700

    Update Color Detection

--- Modbus.py
+++
@@ -1,8 +0,0 @@
-
-from pyModbusTCP.client import ModbusClient
-c = ModbusClient(host="127.0.0.1", port=502, unit_id=1, auto_open=True)
-
-while True:
-    x = input()
-    c.write_single_register(1, int(x))
-
